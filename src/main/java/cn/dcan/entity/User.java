4d22bf3e59b096a831469a7aa31f02819cd3b064
DongCanRola <DongCanThis@outlook.com>
2018-04-19 15:56:20 +0800

    base config

---
+++ src/main/java/cn/dcan/entity/User.java
@@ -0,0 +1,73 @@
+package cn.dcan.entity;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String qqnumber;
+
+    private String wechat;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getQqnumber() {
+        return qqnumber;
+    }
+
+    public void setQqnumber(String qqnumber) {
+        this.qqnumber = qqnumber == null ? null : qqnumber.trim();
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat == null ? null : wechat.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
---
+++ src/test/java/cn/dcan/ServicebsfApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.dcan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicebsfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
