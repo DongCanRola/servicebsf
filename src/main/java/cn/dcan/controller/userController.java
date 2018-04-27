10beba659ed8d88a17e3c0e3e5974fb349778621
DongCanRola <DongCanThis@outlook.com>
2018-04-27 21:14:48 +0800

    exploration 0427

--- src/main/java/cn/dcan/controller/userController.java
+++ src/main/java/cn/dcan/controller/userController.java
@@ -1,9 +1,14 @@
 package cn.dcan.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.core.Response;
+
+import cn.dcan.constrain.SimpleResponse;
+import cn.dcan.constrain.DataResponse;
 
 /**
  * Created by dongc_000 on 2018/4/26.
@@ -17,10 +22,10 @@ public class userController {
         return "hello!";
     }
 
-    /*
-    public Response login () {
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public Response login(@HeaderParam("userName") int id,@HeaderParam("password") String password) {
 
+        return Response.ok(new SimpleResponse(SimpleResponse.ERROR,"登录失败！")).build();
     }
-     */
 
 }
